5c2e85c422ba6f7e4bdd3a48a42ca4906b6c813d
Algan <alganpc@gmail.com>
2024-10-15 15:03:26 +0200

    premier commit one million

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/**/*.{html,js}","*.html"],
+  theme: {
+    extend: {
+      colors:{
+        textcolor:'#F74C3C'
+        fontcolor:'#333333'
+      }
+    }, 
+  },
+  plugins: [],
+}
+
